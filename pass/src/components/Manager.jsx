bf54dce096078be8370e50cbe84a658215072175
Asmi <ami.work015@gmail.com>
2026-05-26 18:49:25 +0530

    swap

--- pass/src/components/Manager.jsx
+++ pass/src/components/Manager.jsx
@@ -79,7 +79,7 @@ const copyToClipboard = (text) => {
             <div className="relative">
               <input ref={passwordRef} value={form.password} onChange={handleChange} className="rounded-full border border-purple-600 w-full p-2 py-1" type="password" placeholder='Enter Password' name="password"></input>
               <span className= 'absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer'onClick={showPassword}>
-                {passwordRef.current?.type === "password" ? "🙈" : "👁️" }
+                {passwordRef.current?.type === "password" ?"👁️" : "🙈" }
               </span>
             </div>
           </div>
